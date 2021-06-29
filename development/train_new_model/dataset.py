6e6ab8c71ac1aa232257cf273b330ae6d5263077
ohjho <jho.yvr+git@gmail.com>
2021-06-29 14:46:40 +0800

    updated training script to accept multiple images

--- development/train_new_model/dataset.py
+++ development/train_new_model/dataset.py
@@ -1,5 +1,5 @@
-import cv2
-
+import cv2, os
+import numpy as np
 import torch
 from torch.utils.data import Dataset
 from instafilter.utils import features_from_image
@@ -8,13 +8,32 @@ from instafilter.utils import features_from_image
 class ColorizedDataset(Dataset):
     def __init__(self, f_source, f_target, device):
 
-        img0 = cv2.imread(str(f_source))
-        f0 = features_from_image(img0)
+        if os.path.isfile(f_source) and os.path.isfile(f_target):
+            # load single images
+            img0 = cv2.imread(str(f_source))
+            f0 = features_from_image(img0)
 
-        img1 = cv2.imread(str(f_target))
-        f1 = features_from_image(img1)
+            img1 = cv2.imread(str(f_target))
+            f1 = features_from_image(img1)
+        else:
+            l_im_fps = [f for f in os.listdir(f_source) if f.endswith(('.jpg','.JPG'))]
+            assert len(l_im_fps)>0, f"not jpg nor JPG files found in {f_source}"
+
+            f0_all = [features_from_image(
+                        cv2.imread(os.path.join(f_source, f))
+                        )
+                for f in l_im_fps]
+            f1_all = [features_from_image(
+                        cv2.imread(os.path.join(f_target, f))
+                        )
+                for f in l_im_fps]
+            f0 = np.concatenate(f0_all, axis = 0)
+            f1 = np.concatenate(f1_all, axis = 0)
 
         assert f0.shape == f1.shape
+        print('--- ColorizedDataset:')
+        print(f"\tinput dataset shape: {f0.shape}")
+        print(f"\ttarget dataset shape: {f1.shape}")
 
         self.x = torch.tensor(f0).to(device)
         self.y = torch.tensor(f1).to(device)
--- development/train_new_model/train.py
+++ development/train_new_model/train.py
@@ -1,6 +1,6 @@
 from tqdm import tqdm
 from pathlib import Path
-import torch
+import torch, os, sys, argparse
 from torch.utils.data import DataLoader
 
 import instafilter
@@ -64,25 +64,93 @@ def train_image_pair(
 
     torch.save(net.state_dict(), f_save_model)
 
+def train_images( d_source, d_target, f_save_model, batch_size=2 ** 10,
+    n_epochs=30, max_learning_rate=0.01):
+    '''
+    Args:
+        d_source: directory of the source images
+        d_target: directory of the target images
+        f_save_model: filepath of the output model
+    '''
+    assert os.path.isdir(d_source), "d_source is not a valid directory"
+    assert os.path.isdir(d_target), "d_target is not a valid directory"
+    # make sure all source and target images are aligned
+    file_check = [os.path.isfile(os.path.join(d_target, f))
+        for f in os.listdir(d_source) if f.endswith(('.jpg','.JPG'))]
+    assert all(file_check), "not all files in d_source is matched with all files in d_target"
+
+    data = ColorizedDataset(f_source = d_source, f_target = d_target, device=device)
+    train_loader = DataLoader(data, batch_size=batch_size, shuffle=True)
+
+    net = ColorNet()
+    criterion = torch.nn.L1Loss()
+    optimizer = loss_func(net.parameters())
+
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        optimizer,
+        max_lr=max_learning_rate,
+        steps_per_epoch=len(train_loader),
+        epochs=n_epochs,
+    )
+
+    net.to(device)
+    net.train()
+
+    for epoch in tqdm(range(n_epochs)):
+        # monitor training loss
+        train_loss = 0.0
+
+        for data, target in train_loader:
+
+            optimizer.zero_grad()
+
+            output = net(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+            # update running training loss
+            train_loss += loss.item() * data.size(0)
+
+        train_loss = train_loss / len(train_loader.dataset)
+        print(f"Epoch: {epoch+1} \tTraining Loss: {train_loss:.6f}")
+
+    torch.save(net.state_dict(), f_save_model)
 
 if __name__ == "__main__":
-
+    parser = argparse.ArgumentParser(description = "Instafilter: simple color NN to create instagram-like filters")
+    parser.add_argument("--source_dir", required = False, type = str, default = None,
+							help = 'directory of images before transformation')
+	parser.add_argument("--target_dir", required = False, type = str, default = None,
+							help = 'directory of images AFTER transformation')
+    parser.add_argument("--model_name", required = False, type = str, default = None,
+							help = 'output filter name (.pt not neccessary)')
+    args = parser.parse_args()
     model_location = module_location / "models"
-    f_source = "input/Normal.jpg"
 
-    for f_target in Path("input").glob("*.jpg"):
+    if args.source_dir and args.target_dir:
+        f_model = os.path.join(model_location, args.model_name + '.pt') if args.model_name else \
+            os.path.join(model_location, os.path.basename(args.target_dir) + '.pt')
+        print(f'Training model {os.path.basename(f_model)} with images in {args.source_dir} and {args.target_dir}')
+        train_images(d_source = args.source_dir, d_target= args.target_dir,
+            f_save_model= f_model)
+    else:
+        f_source = "input/Normal.jpg"
 
-        if "Normal.jpg" in str(f_target):
-            continue
+        for f_target in tqdm(Path("input").glob("*.jpg"), desc = f"looking for models to train in {Path("input")}"):
 
-        f_model = model_location / f_target.name.replace(".jpg", ".pt")
+            if "Normal.jpg" in str(f_target):
+                continue
 
-        if f_target.name == f_source:
-            continue
+            f_model = model_location / f_target.name.replace(".jpg", ".pt")
 
-        if f_model.exists():
-            continue
+            if f_target.name == f_source:
+                continue
 
-        print("Training", f_model)
+            if f_model.exists():
+                continue
 
-        train_image_pair(f_source, f_target, f_model)
+            print("Training", f_model)
+
+            train_image_pair(f_source, f_target, f_model)
